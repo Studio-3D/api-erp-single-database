66db66a36eb0fff48d6d1544fff920116c0dd27a
Mohamed CHAABA <88441166+flavio-back-end@users.noreply.github.com>
2023-05-30 15:05:12 +0100

    Update Immeuble.php

--- app/Models/Immeuble.php
+++ app/Models/Immeuble.php
@@ -9,13 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Immeuble extends Model
 {
     use HasFactory, SoftDeletes;
-    protected $fillable = [
-        'nom',
-        'titre_foncier',
-        'projet_id',
-        'tranche_id',
-        'bloc_id',
-    ];
+     protected $table = 'immeubles';
     protected $dates = ['deleted_at'];
     public function projet()
     {
