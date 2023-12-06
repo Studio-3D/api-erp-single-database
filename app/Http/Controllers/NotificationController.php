906a6141719a7c4e6c39468f05b34e5f8c0bd5f0
FADWA-GHRISSI <47297132+FADWA-GHRISSI@users.noreply.github.com>
2023-12-06 12:05:04 +0100

    Update NotificationController.php

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -23,18 +23,18 @@ class NotificationController extends Controller
             $perPage=$request->input('pageSize',config('app.default_item_number_perpage'));
             $page=$request->input('page',1);
             $all_notifications=[];
-            $notifications_new=0;
+            $new_notifications_count=0;
             $data_get=$this->get_notifications($request,$projet_id);
             foreach($data_get->original as $key => $v){
                 if($key=='all_notifications'){
                     $all_notifications = PaginationHelper::paginate_array($v->toArray(),$perPage,$page,$request->url());
                 }
-                if($key=='notifications_new'){
-                    $notifications_new=$v;
+                if($key=='new_notifications_count'){
+                    $new_notifications_count=$v;
                 }
             }
 
-            return response()->json(['all_notifications'=>$all_notifications,'notifications_new'=>$notifications_new],200);
+            return response()->json(['all_notifications'=>$all_notifications,'new_notifications_count'=>$new_notifications_count],200);
         }
         return response()->json(['error'=>'Unauthorized'],401);
     }
@@ -55,7 +55,7 @@ class NotificationController extends Controller
 
                 $rel_visites=Relance_Rdv_visite::on('temp')->select('relances_rdv_visites.*')->join('visites','visites.id', '=', 'relances_rdv_visites.visite_id') ->select('relances_rdv_visites.*')->where('visites.etat',1)->where('visites.projet_id',$projet_id)->whereDate('relances_rdv_visites.date_relance', '<=', Carbon::now())->where('relances_rdv_visites.user_id', Auth::guard('api')->user()->id)->where('relances_rdv_visites.type_traitement', 0)->where('relances_rdv_visites.type', 1)->orderby('relances_rdv_visites.date_relance', 'asc') ->paginate($perPage, ['*'], 'page', $page);
             }
-           return response()->json(['rel_visites' => $rel_visites]);
+           return response()->json(['relance_visites' => $rel_visites]);
         }
          else{
             return response()->json(['error' => 'Unauthorized'], 401);
@@ -102,7 +102,7 @@ class NotificationController extends Controller
                 $rel_visites=Relance_Rdv_visite::on('temp')->join('visites','visites.id', '=', 'relances_rdv_visites.visite_id')->where('visites.etat',1)->where('visites.projet_id',$projet_id)->whereDate('relances_rdv_visites.date_relance', '<=', Carbon::now())->where('relances_rdv_visites.user_id', Auth::guard('api')->user()->id)->where('relances_rdv_visites.type_traitement', 0)->where('relances_rdv_visites.type', 1)->orderby('relances_rdv_visites.date_relance', 'asc') ->count();
                 $rdv_visites=Relance_Rdv_visite::on('temp')->join('visites','visites.id', '=', 'relances_rdv_visites.visite_id')->where('visites.etat',1)->where('visites.projet_id',$projet_id)->whereDate('relances_rdv_visites.rdv', '<=',Carbon::now())->where('relances_rdv_visites.user_id', Auth::guard('api')->user()->id)->where('relances_rdv_visites.type_traitement', 0)->where('relances_rdv_visites.type', 2)->orderby('relances_rdv_visites.rdv', 'asc')->count();
             }
-           return response()->json(['rel_visites' => $rel_visites,'rdv_visites' => $rdv_visites]);
+           return response()->json(['relance_visites' => $rel_visites,'rdv_visites' => $rdv_visites]);
         }
          else{
             return response()->json(['error' => 'Unauthorized'], 401);
@@ -116,24 +116,24 @@ class NotificationController extends Controller
             if(RoleHelper::AdminSup()){
 
                $all_notifications=Notification::on('temp')->with('prospect','user')->where('projet_id',$projet_id)->withTrashed()->orderBy('date','desc')->get();
-
-               foreach($all_notifications as $notif){
+               $new_notifications_count=Notification::on('temp')->where('projet_id',$projet_id)->where('deleted_at',null)->count();
+               /*foreach($all_notifications as $notif){
                 if($notif->deleted_at==null){
                     $i+=1;
                 }
                }
-               $notifications_new =$i;
-               $notification_new=Notification::on('temp')->where('projet_id',$projet_id)->get();
+               $new_notifications_count =$i;*/
             }else{
                 $all_notifications=Notification::on('temp')->with('prospect','user')->where('projet_id',$projet_id)->where('user_id',Auth::guard('api')->user()->id)->withTrashed()->orderBy('date','desc')->get();
-                foreach($all_notifications as $notif){
+                $new_notifications_count=Notification::on('temp')->where('projet_id',$projet_id)->where('deleted_at',null)->where('user_id',Auth::guard('api')->user()->id)->count();
+                /* foreach($all_notifications as $notif){
                     if($notif->deleted_at==null){
                         $i+=1;
                     }
                 }
-               $notifications_new =$i;
+               $new_notifications_count =$i;*/
                 }
-           return response()->json(['all_notifications' => $all_notifications,'notifications_new'=>$notifications_new]);
+           return response()->json(['all_notifications' => $all_notifications,'new_notifications_count'=>$new_notifications_count]);
         }
          else{
             return response()->json(['error' => 'Unauthorized'], 401);
