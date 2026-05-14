e23d6b787b1fdcbc62ebd89243b2f9b6b1e97a13
airout <a.airout@gmail.com>
2026-05-14 18:52:24 +0100

    manage jobs in kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,18 +13,27 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
 
-        $schedule->command('app:delete-societe-database-command')->everyMinute();
+        $schedule->command('app:delete-societe-database-command')->dailyAt('01:00');
         $schedule->command('app:clear-proposition-table')
             ->everyTenMinutes()
             ->withoutOverlapping(300)
             ->runInBackground();
-         $schedule->command('app:clear-proposition-table')->everyMinute();
-         $schedule->command('app:liberer_bien_pre_reserve')->everyMinute();
+         //$schedule->command('app:clear-proposition-table')->everyMinute();
+        $schedule->command('app:liberer_bien_pre_reserve')
+            ->everyMinute()
+            ->withoutOverlapping(300)
+            ->runInBackground();
         $schedule->command('app:destroy_notif')->dailyAt('00:00');
         $schedule->command(command: 'emails:send-scheduled')->dailyAt('07:00'); // Exécute tous les jours à minuit
         $schedule->command(command: 'app:echeance-email')->dailyAt('07:00'); // Exécute tous les jours à minuit
-        $schedule->command('app:import_fichiers')->everyMinute()->withoutOverlapping(300) ->runInBackground();
-        $schedule->command('app:edit_biens_et_titre_foncier_en_masse')->everyMinute();
+        $schedule->command('app:import_fichiers')
+            ->everyTwoMinutes()
+            ->withoutOverlapping(300)
+            ->runInBackground();
+        $schedule->command('app:edit_biens_et_titre_foncier_en_masse')
+            ->everyTwoMinutes()
+            ->withoutOverlapping(300)
+            ->runInBackground();
         $schedule->command('app:clear-webhook_events-table')->sundays()->at('07:00'); // Runs every Sunday at midnight
         $schedule->command(command: 'whatsapp:send-reminder')->dailyAt('00:00'); // Exécute tous les jours à minuit
         $schedule->command('delete_creneau_propose')->everyThreeMinutes();//3min
